38507f99927ff7bacb42dc90224c124acc094b4b
Madhumitha <madhumithavenu03@gmail.com>
2023-11-10 16:47:10 +0530

    Adding header file

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import "./Header.css";
 
-function Header() {
+export default class Header extends Component {
+    render(){
   return (
-    <div>Header</div>
+    <header className='header'>
+      <div>
+        <h1>
+          <Link to='/' className='logo'>
+            Fruit Kart
+          </Link>
+        </h1>
+      </div>
+      <div className='header-links'>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <Link to="/signup">SignUp</Link>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <Link to="/cart" className='cart'>
+            <i className='fas fa-shopping-cart' />
+            <span className='cart-length'>
+             
+            </span>
+            </Link>
+          </li>
+        </ul>
+    
+      </div>
+    </header>
   )
+    }
 }
 
-export default Header
