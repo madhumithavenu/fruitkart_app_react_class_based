989cd46fb9f1de51edc82caa316ea6447a6fb09b
Madhumitha <madhumithavenu03@gmail.com>
2023-11-11 16:52:24 +0530

    importing cart items

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,75 @@
-
 import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Products from './components/products/Products.jsx';
 import SignUp from './components/signup/SignUp.jsx';
 import Cart from './components/cart/Cart.jsx';
+import { Component, useState } from 'react';
 import Header from './components/header/Header.jsx';
-import { Component } from 'react';
 
 export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+     listcart:{
+      cartItems: null, 
+      setCartItems: null
+   }
+ }
+}
+ 
+
+  handleAddProduct(product) {
+    const productExist = this.cartItems.find(item => {
+      return item.id === product.id;
+    });
+
+    if (productExist) {
+      this.setCartItems(this.cartItems.map(item => {
+        return item.id === product.id ?
+          { ...productExist, quantity: productExist.quantity + 1 } :
+          item;
+      }))
+    }
+    else {
+      this.setCartItems([this.cartItems, { ...product, quantity: 1 }]);
+    }
+  }
+
+  handleRemoveProduct(product) {
+    const productExist = this.cartItems.find(item => {
+      return item.id === product.id;
+    });
+
+    if (productExist.quantity === 1) {
+      this.setCartItems(this.cartItems.filter(item => {
+        return item.id !== product.id;
+      }));
+    }
+    else {
+      this.setCartItems(this.cartItems.map(item => {
+        return item.id === product.id ?
+          { ...productExist, quantity: productExist.quantity - 1 } :
+          item
+      }))
+    }
+  }
+
+  handleCartClearence() {
+    this.setCartItems([]);
+  }
  render(){
+  const {cartItems, setCartItems}=  this.state.listcart;
   return (
-    <BrowserRouter>
-      <Header cartItems={null} />
-      <Routes>
-      <Route exact path="/" element={<Products />} />
-        <Route exact path="/signup" element={<SignUp />} />
-        <Route exact path="/cart" element={<Cart />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <Header cartItems={cartItems} />
+        <Routes>
+          <Route exact path="/" element={<Products handleAddProduct={this.handleAddProduct.bind(this)} />} />
+          <Route exact path="/signup" element={<SignUp />} />
+          <Route exact path="/cart" element={<Cart cartItems={cartItems} handleAddProduct={this.handleAddProduct.bind(this)} handleRemoveProduct={this.handleRemoveProduct.bind(this)} handleCartClearence={this.handleCartClearence.bind(this)} />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 }
-
--- frontend/src/components/cart/Cart.jsx
+++ frontend/src/components/cart/Cart.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { Component } from 'react';
+import './Cart.css';
 
-function Cart() {
-  return (
-    <div>Cart</div>
-  )
+export default class Cart extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      listcart:{
+        cartItems: null,
+         handleAddProduct: null,
+         handleRemoveProduct: null,
+         handleCartClearence: null
+      }
+ }
 }
+   totalPrice = this.cartItems.reduce((price, item) => {
+    return price + (this.item.quantity * this.item.price)
+  }, 0);
 
-export default Cart
+  render(){
+  const {cartItems} = this.state.listcart;
+
+  return (
+    <div className='cart-items'>
+      <h2 className='cart-items-header'>Cart Items</h2>
+      <div className='clear-cart'>
+        {cartItems.length >= 1 && (
+          <button className='clear-cart-button' onClick={this.handleCartClearence}>Clear Cart</button>
+        )}
+      </div>
+      {cartItems.length === 0 &&
+        <div className='cart-items-empty'>
+          No items are added in the cart
+        </div>}
+      <div>
+        {cartItems.map(item => {
+          return (<div key={item.id} className="cart-items-list">
+            <img className="cart-item-image"
+              src={item.image}
+              alt={item.name} />
+            <div className='cart-items-name'>
+              {item.name}
+            </div>
+            <div className='cart-items-function'>
+              <button className='cart-items-add' onClick={() => this.handleAddProduct(item)}>+</button>
+              <button className='cart-items-remove' onClick={() => this.handleRemoveProduct(item)}>-</button>
+            </div>
+            <div className='cart-items-price'>
+              {item.quantity} x Rs.{item.price}
+            </div>
+          </div>
+          );
+        })
+        }
+      </div>
+      <div className='cart-items-total-price-name'>
+        Total Price
+        <div className='cart-items-total-price'> Rs.{this.cartItems.reduce((price, item) => {
+    return price + (this.item.quantity * this.item.price)
+  }, 0)}</div>
+      </div>
+    </div>
+  );
+}
+}
